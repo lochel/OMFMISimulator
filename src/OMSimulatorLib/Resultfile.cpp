a380c9f486b6fc3a47a9934465de0f58284fd087
Lennart Ochel <lennart.ochel@liu.se>
2017-08-17 12:31:05 +0200

    Replace commas within signal names for csv export (, -> $C)

--- src/OMSimulatorLib/Resultfile.cpp
+++ src/OMSimulatorLib/Resultfile.cpp
@@ -40,6 +40,20 @@
 #include <stdlib.h>
 #include <string>
 
+// https://stackoverflow.com/a/3418285
+void replaceAll(std::string& str, const std::string& from, const std::string& to)
+{
+  if (from.empty())
+    return;
+
+  size_t start_pos = 0;
+  while ((start_pos = str.find(from, start_pos)) != std::string::npos)
+  {
+    str.replace(start_pos, from.length(), to);
+    start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
+  }
+}
+
 Resultfile::Resultfile(std::string filename, fmi2_import_t* fmu)
 {
   this->fmu = fmu;
@@ -54,6 +68,7 @@ Resultfile::Resultfile(std::string filename, fmi2_import_t* fmu)
   {
     fmi2_import_variable_t* var = fmi2_import_get_variable(list, i);
     std::string name = fmi2_import_get_variable_name(var);
+    replaceAll(name, ",", "$C");
     resultFile << ", " << name;
   }
 
